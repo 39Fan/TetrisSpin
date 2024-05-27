e5a1fe036067738044d02c25a44d0198358c3a61
Tomoya Hashida <tomoya.hashida@gmail.com>
2024-05-27 18:45:45 +0900

    [add] Existence enumの追加

--- TetrisSpin/Assets/Scripts/SpinCheck.cs
+++ TetrisSpin/Assets/Scripts/SpinCheck.cs
@@ -12,10 +12,10 @@ using UnityEngine;
 public class SpinCheck : MonoBehaviour
 {
     // // ミノの向き //
-    // private string North = "North";
-    // private string East = "East";
-    // private string South = "South";
-    // private string West = "West";
+    // private Existence North = "North";
+    // private Existence East = "East";
+    // private Existence South = "South";
+    // private Existence West = "West";
 
     // Spinの種類 //
     [SerializeField] private SpinTypeNames SpinTypeName;
@@ -131,7 +131,7 @@ public class SpinCheck : MonoBehaviour
 
 
         // 指定マスのブロックの有無情報を格納するリスト
-        List<string> checkBlocks_ForJ = new List<string>();
+        List<Existence> checkBlocks_ForJ = new List<Existence>();
 
         // 各向きに対応したオフセット //
         int[,] northOffset_ForJ = new int[,]
@@ -179,16 +179,16 @@ public class SpinCheck : MonoBehaviour
         {
             if (board.CheckGrid(pos_x + currentOffsets[grid, 0], pos_y + currentOffsets[grid, 1], spawner.activeMino))
             {
-                checkBlocks_ForJ.Add("Exist");
+                checkBlocks_ForJ.Add(Existence.Exist);
             }
             else
             {
-                checkBlocks_ForJ.Add("Not Exist");
+                checkBlocks_ForJ.Add(Existence.NotExist);
             }
         }
 
         // 条件を満たすか確認(②の確認込み)
-        if (checkBlocks_ForJ.FindAll(block => block == "Exist").Count >= 3 && gameStatus.stepsSRS >= 1)
+        if (checkBlocks_ForJ.FindAll(block => block == Existence.Exist).Count >= 3 && gameStatus.stepsSRS >= 1)
         {
             SpinTypeName = SpinTypeNames.J_Spin;
         }
@@ -213,7 +213,7 @@ public class SpinCheck : MonoBehaviour
 
 
         // 指定マスのブロックの有無情報を格納するリスト
-        List<string> checkBlocks_ForJ = new List<string>();
+        List<Existence> checkBlocks_ForJ = new List<Existence>();
 
         // 各向きに対応したオフセット //
         int[,] northOffset_ForJ = new int[,]
@@ -262,16 +262,16 @@ public class SpinCheck : MonoBehaviour
         {
             if (board.CheckGrid(pos_x + currentOffsets[grid, 0], pos_y + currentOffsets[grid, 1], spawner.activeMino))
             {
-                checkBlocks_ForJ.Add("Exist");
+                checkBlocks_ForJ.Add(Existence.Exist);
             }
             else
             {
-                checkBlocks_ForJ.Add("Not Exist");
+                checkBlocks_ForJ.Add(Existence.NotExist);
             }
         }
 
         // 条件を満たすか確認(②の確認込み)
-        if (checkBlocks_ForJ.FindAll(block => block == "Exist").Count >= 3 && gameStatus.stepsSRS >= 1)
+        if (checkBlocks_ForJ.FindAll(block => block == Existence.Exist).Count >= 3 && gameStatus.stepsSRS >= 1)
         {
             SpinTypeName = SpinTypeNames.L_Spin;
         }
@@ -296,7 +296,7 @@ public class SpinCheck : MonoBehaviour
     //     //               ▫️           ▫️          ▫️       // ◆はJミノの軸
 
 
-    //     List<string> checkBlocks_ForJ = new List<string>(); // IspinMiniの判定に必要なリスト
+    //     List<Existence> checkBlocks_ForJ = new List<Existence>(); // IspinMiniの判定に必要なリスト
 
     //     // 各向きに対応したオフセット //
     //     int[,] northOffset_ForJ = new int[,]
@@ -330,7 +330,7 @@ public class SpinCheck : MonoBehaviour
     //             }
     //             else
     //             {
-    //                 checkBlocks_ForJ.Add("Not Exist");
+    //                 checkBlocks_ForJ.Add(Existence.NotExist);
     //             }
     //         }
     //     }
@@ -345,7 +345,7 @@ public class SpinCheck : MonoBehaviour
     //             }
     //             else
     //             {
-    //                 checkBlocks_ForJ.Add("Not Exist");
+    //                 checkBlocks_ForJ.Add(Existence.NotExist);
     //             }
     //         }
     //     }
@@ -360,7 +360,7 @@ public class SpinCheck : MonoBehaviour
     //             }
     //             else
     //             {
-    //                 checkBlocks_ForJ.Add("Not Exist");
+    //                 checkBlocks_ForJ.Add(Existence.NotExist);
     //             }
     //         }
     //     }
@@ -375,7 +375,7 @@ public class SpinCheck : MonoBehaviour
     //             }
     //             else
     //             {
-    //                 checkBlocks_ForJ.Add("Not Exist");
+    //                 checkBlocks_ForJ.Add(Existence.NotExist);
     //             }
     //         }
     //     }
@@ -396,8 +396,8 @@ public class SpinCheck : MonoBehaviour
             // ▫️▫️▫️▫️ ←②
 
             // 指定マスのブロックの有無情報を格納するリスト
-            List<string> checkBlocksAbove_ForI = new List<string>();
-            List<string> checkBlocksBelow_ForI = new List<string>();
+            List<Existence> checkBlocksAbove_ForI = new List<Existence>();
+            List<Existence> checkBlocksBelow_ForI = new List<Existence>();
 
             int yOffset = 1; // IspinMiniの判定に必要なオフセット
 
@@ -409,11 +409,11 @@ public class SpinCheck : MonoBehaviour
                 // 1マス上部にブロックがあるか調べる
                 if (board.CheckGrid(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y + yOffset), spawner.activeMino))
                 {
-                    checkBlocksAbove_ForI.Add("Exist");
+                    checkBlocksAbove_ForI.Add(Existence.Exist);
                 }
                 else
                 {
-                    checkBlocksAbove_ForI.Add("Not Exist");
+                    checkBlocksAbove_ForI.Add(Existence.NotExist);
                 }
             }
 
@@ -425,11 +425,11 @@ public class SpinCheck : MonoBehaviour
                 // 1マス上部にブロックがあるか調べる
                 if (board.CheckGrid(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y - yOffset), spawner.activeMino))
                 {
-                    checkBlocksBelow_ForI.Add("Exist");
+                    checkBlocksBelow_ForI.Add(Existence.Exist);
                 }
                 else
                 {
-                    checkBlocksBelow_ForI.Add("Not Exist");
+                    checkBlocksBelow_ForI.Add(Existence.NotExist);
                 }
             }
 
@@ -441,8 +441,8 @@ public class SpinCheck : MonoBehaviour
             LogHelper.Log(LogHelper.LogLevel.Info, "SpinCheck", "IspinCheck()", checkBlocksBelowInfo); // Infoログ
 
             // 条件を満たすか確認
-            if (checkBlocksAbove_ForI.FindAll(block => block == "Exist").Count >= 1 ||
-                checkBlocksBelow_ForI.FindAll(block => block == "Exist").Count >= 3)
+            if (checkBlocksAbove_ForI.FindAll(block => block == Existence.Exist).Count >= 1 ||
+                checkBlocksBelow_ForI.FindAll(block => block == Existence.Exist).Count >= 3)
             {
                 SpinTypeName = SpinTypeNames.I_SpinMini;
             }
@@ -469,9 +469,9 @@ public class SpinCheck : MonoBehaviour
 
 
             // 指定マスのブロックの有無情報を格納するリスト
-            List<string> checkBlocksRightSide_ForI = new List<string>();
-            List<string> checkBlocksLeftSide_ForI = new List<string>();
-            List<string> checkBlocksUpper_ForI = new List<string>();
+            List<Existence> checkBlocksRightSide_ForI = new List<Existence>();
+            List<Existence> checkBlocksLeftSide_ForI = new List<Existence>();
+            List<Existence> checkBlocksUpper_ForI = new List<Existence>();
 
             // Iミノの上部にブロックがあるか調べるために必要な変数
             int IminoTopPosition_y = board.CheckActiveMinoTopBlockPosition_y(spawner.activeMino); // Iミノの最上部のy座標
@@ -487,11 +487,11 @@ public class SpinCheck : MonoBehaviour
                 // 1マス右側にブロックがあるか調べる
                 if (board.CheckGrid(Mathf.RoundToInt(pos.x + xOffset), Mathf.RoundToInt(pos.y), spawner.activeMino))
                 {
-                    checkBlocksRightSide_ForI.Add("Exist");
+                    checkBlocksRightSide_ForI.Add(Existence.Exist);
                 }
                 else
                 {
-                    checkBlocksRightSide_ForI.Add("Not Exist");
+                    checkBlocksRightSide_ForI.Add(Existence.NotExist);
                 }
             }
             foreach (Transform item in spawner.activeMino.transform) // Iミノを構成するブロックそれぞれを確認する
@@ -501,11 +501,11 @@ public class SpinCheck : MonoBehaviour
                 // 1マス左側にブロックがあるか調べる
                 if (board.CheckGrid(Mathf.RoundToInt(pos.x - xOffset), Mathf.RoundToInt(pos.y), spawner.activeMino))
                 {
-                    checkBlocksLeftSide_ForI.Add("Exist");
+                    checkBlocksLeftSide_ForI.Add(Existence.Exist);
                 }
                 else
                 {
-                    checkBlocksLeftSide_ForI.Add("Not Exist");
+                    checkBlocksLeftSide_ForI.Add(Existence.NotExist);
                 }
             }
 
@@ -515,11 +515,11 @@ public class SpinCheck : MonoBehaviour
                 // Iミノの上部にブロックが存在するか1マスずつ調べていく
                 if (board.CheckGrid(Mathf.RoundToInt(spawner.activeMino.transform.position.x), IminoTopPosition_y + yOffset, spawner.activeMino))
                 {
-                    checkBlocksUpper_ForI.Add("Exist");
+                    checkBlocksUpper_ForI.Add(Existence.Exist);
                 }
                 else
                 {
-                    checkBlocksUpper_ForI.Add("Not Exist");
+                    checkBlocksUpper_ForI.Add(Existence.NotExist);
                 }
 
                 yOffset++;
@@ -536,9 +536,9 @@ public class SpinCheck : MonoBehaviour
             LogHelper.Log(LogHelper.LogLevel.Info, "SpinCheck", "IspinCheck()", checkBlocksUpperInfo); // Infoログ
 
             // 条件を満たすか確認
-            if (checkBlocksRightSide_ForI.FindAll(block => block == "Exist").Count >= 3 &&
-                checkBlocksLeftSide_ForI.FindAll(block => block == "Exist").Count >= 3 &&
-                checkBlocksUpper_ForI.FindAll(block => block == "Exist").Count >= 1)
+            if (checkBlocksRightSide_ForI.FindAll(block => block == Existence.Exist).Count >= 3 &&
+                checkBlocksLeftSide_ForI.FindAll(block => block == Existence.Exist).Count >= 3 &&
+                checkBlocksUpper_ForI.FindAll(block => block == Existence.Exist).Count >= 1)
             {
                 SpinTypeName = SpinTypeNames.I_Spin;
             }
@@ -550,13 +550,13 @@ public class SpinCheck : MonoBehaviour
     {
         // Tミノの中心から順番に[1, 1]、[1, -1]、[-1, 1]、[-1, -1]の分だけ移動した座標にブロックや壁がないか確認する
         // ブロックや壁があった時は Exist ない時は Not Exist で_ActiveMinoCountのリストに追加されていく
-        // 例: checkBlocks_ForT["Exist", "Exist", "Not Exist", "Exist"]
+        // 例: checkBlocks_ForT["Exist", "Exist", Existence.NotExist, "Exist"]
         // ▫️▪️▫️
         // ▪️▪️▪️
         // ▫️ ▫️  ←四隅を調べる
 
         // 指定マスのブロックの有無情報を格納するリスト
-        List<string> checkBlocks_ForT = new List<string>();
+        List<Existence> checkBlocks_ForT = new List<Existence>();
 
         for (int x = 1; x >= -1; x -= 2)
         {
@@ -564,11 +564,11 @@ public class SpinCheck : MonoBehaviour
             {
                 if (board.CheckGrid((int)Mathf.Round(spawner.activeMino.transform.position.x) + x, (int)Mathf.Round(spawner.activeMino.transform.position.y) + y, spawner.activeMino))
                 {
-                    checkBlocks_ForT.Add("Exist");
+                    checkBlocks_ForT.Add(Existence.Exist);
                 }
                 else
                 {
-                    checkBlocks_ForT.Add("Not Exist");
+                    checkBlocks_ForT.Add(Existence.NotExist);
                 }
             }
         }
@@ -594,7 +594,7 @@ public class SpinCheck : MonoBehaviour
         // ▪️▪️▪️
         // ▫️ ▫️
 
-        if (checkBlocks_ForT.FindAll(block => block == "Exist").Count >= 3) // 3マス以上ブロックや壁に埋まっている場合
+        if (checkBlocks_ForT.FindAll(block => block == Existence.Exist).Count >= 3) // 3マス以上ブロックや壁に埋まっている場合
         {
             if (gameStatus.stepsSRS == 4) // SRSが4段階の時は T-Spin 判定になる
             {
@@ -611,7 +611,7 @@ public class SpinCheck : MonoBehaviour
                 switch (gameStatus.minoAngleAfter)
                 {
                     case MinoDirections.North: // Tミノが北向きの時、右上と左上を確認する
-                        if (checkBlocks_ForT[right_up] == "Not Exist" || checkBlocks_ForT[left_up] == "Not Exist")
+                        if (checkBlocks_ForT[right_up] == Existence.NotExist || checkBlocks_ForT[left_up] == Existence.NotExist)
                         {
                             SpinTypeName = SpinTypeNames.T_SpinMini;
                         }
@@ -623,7 +623,7 @@ public class SpinCheck : MonoBehaviour
                         break;
 
                     case MinoDirections.East: // 右上と右下
-                        if (checkBlocks_ForT[right_up] == "Not Exist" || checkBlocks_ForT[right_down] == "Not Exist")
+                        if (checkBlocks_ForT[right_up] == Existence.NotExist || checkBlocks_ForT[right_down] == Existence.NotExist)
                         {
                             SpinTypeName = SpinTypeNames.T_SpinMini;
                         }
@@ -635,7 +635,7 @@ public class SpinCheck : MonoBehaviour
                         break;
 
                     case MinoDirections.South: // 右下と左下
-                        if (checkBlocks_ForT[right_down] == "Not Exist" || checkBlocks_ForT[left_down] == "Not Exist")
+                        if (checkBlocks_ForT[right_down] == Existence.NotExist || checkBlocks_ForT[left_down] == Existence.NotExist)
                         {
                             SpinTypeName = SpinTypeNames.T_SpinMini;
                         }
@@ -647,7 +647,7 @@ public class SpinCheck : MonoBehaviour
                         break;
 
                     case MinoDirections.West: // 左上と左下
-                        if (checkBlocks_ForT[left_up] == "Not Exist" || checkBlocks_ForT[left_down] == "Not Exist")
+                        if (checkBlocks_ForT[left_up] == Existence.NotExist || checkBlocks_ForT[left_down] == Existence.NotExist)
                         {
                             SpinTypeName = SpinTypeNames.T_SpinMini;
                         }
