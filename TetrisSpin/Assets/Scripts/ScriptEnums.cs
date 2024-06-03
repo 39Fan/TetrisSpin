421e479675ab46a74b59388ce9984211134e2149
Tomoya Hashida <tomoya.hashida@gmail.com>
2024-06-03 10:41:08 +0900

    [clean]
    可読性向上のため、enumデータを関連性のあるスクリプトに移動させた。

--- TetrisSpin/Assets/Scripts/ScriptEnums.cs
+++ TetrisSpin/Assets/Scripts/ScriptEnums.cs
@@ -1,26 +1,26 @@
-/// <summary>
-/// スクリプト一覧
-/// </summary>
-public enum Scripts
-{
-    AudioManager,
-    Board,
-    GameManager,
-    GameStatus,
-    LogHelper,
-    Mino,
-    Rounding,
-    SceneTransition,
-    Spawner,
-    SpinCheck,
-    TextEffect,
-    Timer
-}
+// /// <summary>
+// /// スクリプト一覧
+// /// </summary>
+// public enum Scripts
+// {
+//     AudioManager,
+//     Board,
+//     GameManager,
+//     GameStatus,
+//     LogHelper,
+//     Mino,
+//     Rounding,
+//     SceneTransition,
+//     Spawner,
+//     SpinCheck,
+//     TextEffect,
+//     Timer
+// }
 
-/// <summary>
-/// 関数一覧
-/// </summary>
-public enum Method
-{
-    //
-}
+// /// <summary>
+// /// 関数一覧
+// /// </summary>
+// public enum Method
+// {
+//     //
+// }
