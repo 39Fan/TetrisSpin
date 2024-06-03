e3ee70843fd1b002ea79c1cdb28e007db3acdcc2
Tomoya Hashida <tomoya.hashida@gmail.com>
2024-06-03 10:43:22 +0900

    [clean]
    TextEffectで処理していたAttackLinesの計算の移行の完了

--- TetrisSpin/Assets/Scripts/AttackCalculator.cs
+++ TetrisSpin/Assets/Scripts/AttackCalculator.cs
@@ -2,82 +2,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// AttackCalculatorの統計情報を保持する構造体
+/// AttackCalculatorの統計情報を保持する静的クラス
 /// </summary>
-public struct AttackCalculatorStats
+public static class AttackCalculatorStats
 {
     /// <summary> BackToBackの判定 </summary>
-    private bool backToBack;
+    private static bool backToBack = false;
 
     /// <summary> PerfectClearの判定 </summary>
     /// <remarks>
     /// ゲーム開始時を除いて、ゲームプレイ中にブロックがなくなったらPerfectClear判定となる
     /// </remarks>
-    private bool perfectClear;
+    private static bool perfectClear = false;
 
     /// <summary> Renの判定 </summary>
     /// <remarks>
     /// 3回連続で列消去すると「2REN」なので、初期値は-1に設定
     /// </remarks>
     /// <value> -1~ </value>
-    private int ren;
+    private static int ren = -1;
 
     /// <summary> 攻撃ライン数 </summary>
     /// <remarks>
     /// ゲームスタート時に攻撃ラインは存在しないので、初期値は0
     /// </remarks>
     /// <value> 0~ </value>
-    [SerializeField] private int attackLines;
+    private static int attackLines = 0;
 
     // ゲッタープロパティ //
-    public bool BackToBack => backToBack;
-    public bool PerfectClear => perfectClear;
-    public int Ren => ren;
-    public int AttackLines => attackLines;
-
-    /// <summary> デフォルトコンストラクタ </summary>
-    public AttackCalculatorStats(bool _backToBack, bool _perfectClear, int _ren, int _attackLines)
-    {
-        backToBack = _backToBack;
-        perfectClear = _perfectClear;
-        ren = _ren;
-        attackLines = _attackLines;
-    }
-
-    /// <summary> デフォルトの <see cref="AttackCalculatorStats"/> を作成する関数 </summary>
-    /// <returns>
-    /// デフォルト値で初期化された <see cref="AttackCalculatorStats"/> のインスタンス
-    /// </returns>
-    public static AttackCalculatorStats CreateDefault()
-    {
-        return new AttackCalculatorStats
-        {
-            backToBack = false,
-            perfectClear = false,
-            ren = -1,
-            attackLines = 0
-        };
-    }
+    public static bool BackToBack => backToBack;
+    public static bool PerfectClear => perfectClear;
+    public static int Ren => ren;
+    public static int AttackLines => attackLines;
 
     /// <summary> 指定されたフィールドの値を更新する関数 </summary>
     /// <param name="_backToBack"> BackToBackの判定 </param>
     /// <param name="_perfectClear"> PerfectClearの判定 </param>
     /// <param name="_ren"> Renの判定 </param>
     /// <param name="_attackLines"> 攻撃ライン数 </param>
-    /// <returns> 更新された <see cref="AttackCalculatorStats"/> の新しいインスタンス </returns>
     /// <remarks>
     /// 指定されていない引数は現在の値を維持
     /// </remarks>
-    public AttackCalculatorStats Update(bool? _backToBack = null, bool? _perfectClear = null, int? _ren = null, int? _attackLines = null)
+    public static void Update(bool? _backToBack = null, bool? _perfectClear = null, int? _ren = null, int? _attackLines = null)
     {
-        var updatedStats = new AttackCalculatorStats(
-            _backToBack ?? backToBack,
-            _perfectClear ?? perfectClear,
-            _ren ?? ren,
-            _attackLines ?? attackLines
-        );
+        backToBack = _backToBack ?? backToBack;
+        perfectClear = _perfectClear ?? perfectClear;
+        ren = _ren ?? ren;
+        attackLines = _attackLines ?? attackLines;
         // TODO: ログの記入
-        return updatedStats;
+    }
+
+    /// <summary> デフォルトの <see cref="AttackCalculatorStats"/> にリセットする関数 </summary>
+    public static void Reset()
+    {
+        backToBack = false;
+        perfectClear = false;
+        ren = -1;
+        attackLines = 0;
     }
 }
 
@@ -152,9 +133,6 @@ public class AttackCalculator : MonoBehaviour
     Board board;
     TextEffect textEffect;
 
-    // 干渉するストラクト //
-    private AttackCalculatorStats attackCalculatorStats;
-
     /// <summary>
     /// AttackCalculatorStatsの初期化を行う
     /// </summary>
@@ -162,8 +140,6 @@ public class AttackCalculator : MonoBehaviour
     {
         board = FindObjectOfType<Board>();
         textEffect = FindObjectOfType<TextEffect>();
-
-        attackCalculatorStats = AttackCalculatorStats.CreateDefault();
     }
 
     /// <summary> 合計攻撃力を計算する関数 </summary>
@@ -172,7 +148,7 @@ public class AttackCalculator : MonoBehaviour
     public void CalculateAttackLines(SpinTypeNames _spinType, int _lineClearCount)
     {
         // Spin判定と消去ライン数に対応した攻撃力を加算する
-        attackCalculatorStats = attackCalculatorStats.Update(_attackLines: attackCalculatorStats.AttackLines + spinTypeAttackMapping[_spinType][_lineClearCount]);
+        AttackCalculatorStats.Update(_attackLines: AttackCalculatorStats.AttackLines + spinTypeAttackMapping[_spinType][_lineClearCount]);
 
         CalculateBackToBack(_spinType, _lineClearCount);
 
@@ -180,7 +156,7 @@ public class AttackCalculator : MonoBehaviour
 
         CalculateRen(_lineClearCount);
 
-        Debug.Log(attackCalculatorStats.AttackLines);
+        Debug.Log(AttackCalculatorStats.AttackLines);
     }
 
     /// <summary> BackToBackの判定と攻撃力ボーナスを計算する関数 </summary>
@@ -191,23 +167,23 @@ public class AttackCalculator : MonoBehaviour
         /// <summary> BackToBackの攻撃力ボーナス </summary>
         int backToBackBonus = 1;
 
-        if (attackCalculatorStats.BackToBack == true &&
+        if (AttackCalculatorStats.BackToBack == true &&
             (_lineClearCount == 4 || (_spinType != SpinTypeNames.None && _lineClearCount >= 1)))
         {
-            attackCalculatorStats = attackCalculatorStats.Update(_attackLines: attackCalculatorStats.AttackLines + backToBackBonus);
+            AttackCalculatorStats.Update(_attackLines: AttackCalculatorStats.AttackLines + backToBackBonus);
 
             textEffect.BackToBackAnimation();
         }
 
         if (_lineClearCount == 4 || _spinType != SpinTypeNames.None)
         {
-            attackCalculatorStats = attackCalculatorStats.Update(_backToBack: true);
+            AttackCalculatorStats.Update(_backToBack: true);
         }
         else
         {
             if (_lineClearCount >= 1)
             {
-                attackCalculatorStats = attackCalculatorStats.Update(_backToBack: false);
+                AttackCalculatorStats.Update(_backToBack: false);
             }
         }
     }
@@ -220,14 +196,14 @@ public class AttackCalculator : MonoBehaviour
 
         if (board.CheckPerfectClear())
         {
-            attackCalculatorStats = attackCalculatorStats.Update(_perfectClear: true);
-            attackCalculatorStats = attackCalculatorStats.Update(_attackLines: perfectClearBonus);
+            AttackCalculatorStats.Update(_perfectClear: true);
+            AttackCalculatorStats.Update(_attackLines: perfectClearBonus);
 
             textEffect.PerfectClearAnimation();
         }
         else
         {
-            attackCalculatorStats = attackCalculatorStats.Update(_perfectClear: false);
+            AttackCalculatorStats.Update(_perfectClear: false);
         }
     }
 
@@ -240,15 +216,99 @@ public class AttackCalculator : MonoBehaviour
 
         if (_lineClearCount >= 1)
         {
-            attackCalculatorStats = attackCalculatorStats.Update(_ren: attackCalculatorStats.Ren + 1);
-            attackCalculatorStats = attackCalculatorStats.Update(_attackLines:
-                attackCalculatorStats.AttackLines + renBonus[attackCalculatorStats.Ren]);
+            AttackCalculatorStats.Update(_ren: AttackCalculatorStats.Ren + 1);
+            AttackCalculatorStats.Update(_attackLines:
+                AttackCalculatorStats.AttackLines + renBonus[AttackCalculatorStats.Ren]);
 
             // TODO TextEffectでRENのテキスト表示をする
         }
         else
         {
-            attackCalculatorStats = attackCalculatorStats.Update(_ren: -1);
+            AttackCalculatorStats.Update(_ren: -1);
         }
     }
 }
+
+/////////////////// 旧コード ///////////////////
+
+// /// <summary>
+// /// AttackCalculatorの統計情報を保持する構造体
+// /// </summary>
+// public struct AttackCalculatorStats
+// {
+//     /// <summary> BackToBackの判定 </summary>
+//     private bool backToBack;
+
+//     /// <summary> PerfectClearの判定 </summary>
+//     /// <remarks>
+//     /// ゲーム開始時を除いて、ゲームプレイ中にブロックがなくなったらPerfectClear判定となる
+//     /// </remarks>
+//     private bool perfectClear;
+
+//     /// <summary> Renの判定 </summary>
+//     /// <remarks>
+//     /// 3回連続で列消去すると「2REN」なので、初期値は-1に設定
+//     /// </remarks>
+//     /// <value> -1~ </value>
+//     private int ren;
+
+//     /// <summary> 攻撃ライン数 </summary>
+//     /// <remarks>
+//     /// ゲームスタート時に攻撃ラインは存在しないので、初期値は0
+//     /// </remarks>
+//     /// <value> 0~ </value>
+//     [SerializeField] private int attackLines;
+
+//     // ゲッタープロパティ //
+//     public bool BackToBack => backToBack;
+//     public bool PerfectClear => perfectClear;
+//     public int Ren => ren;
+//     public int AttackLines => attackLines;
+
+//     /// <summary> デフォルトコンストラクタ </summary>
+//     public AttackCalculatorStats(bool _backToBack, bool _perfectClear, int _ren, int _attackLines)
+//     {
+//         backToBack = _backToBack;
+//         perfectClear = _perfectClear;
+//         ren = _ren;
+//         attackLines = _attackLines;
+//     }
+
+//     /// <summary> デフォルトの <see cref="AttackCalculatorStats"/> を作成する関数 </summary>
+//     /// <returns>
+//     /// デフォルト値で初期化された <see cref="AttackCalculatorStats"/> のインスタンス
+//     /// </returns>
+//     public static AttackCalculatorStats CreateDefault()
+//     {
+//         return new AttackCalculatorStats
+//         {
+//             backToBack = false,
+//             perfectClear = false,
+//             ren = -1,
+//             attackLines = 0
+//         };
+//     }
+
+//     /// <summary> 指定されたフィールドの値を更新する関数 </summary>
+//     /// <param name="_backToBack"> BackToBackの判定 </param>
+//     /// <param name="_perfectClear"> PerfectClearの判定 </param>
+//     /// <param name="_ren"> Renの判定 </param>
+//     /// <param name="_attackLines"> 攻撃ライン数 </param>
+//     /// <returns> 更新された <see cref="AttackCalculatorStats"/> の新しいインスタンス </returns>
+//     /// <remarks>
+//     /// 指定されていない引数は現在の値を維持
+//     /// </remarks>
+//     public AttackCalculatorStats Update(bool? _backToBack = null, bool? _perfectClear = null, int? _ren = null, int? _attackLines = null)
+//     {
+//         var updatedStats = new AttackCalculatorStats(
+//             _backToBack ?? backToBack,
+//             _perfectClear ?? perfectClear,
+//             _ren ?? ren,
+//             _attackLines ?? attackLines
+//         );
+//         // TODO: ログの記入
+//         return updatedStats;
+//     }
+// }
+
+/////////////////////////////////////////////////////////
